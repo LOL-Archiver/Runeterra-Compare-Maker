499068fbbd88fd96b5b7981e7f10117f4130a9d0
DanoR <zheung@vip.qq.com>
2019-01-22 15:35:16 +0800

    reorganize the util

--- index.js
+++ index.js
@@ -10,51 +10,57 @@ let compBiogA = async function(name) {
 	let cnc = (await Axios.get(`https://yz.lol.qq.com/v1/zh_cn/champions/${name}/index.json`)).data.champion;
 	let enc = (await Axios.get(`https://universe-meeps.leagueoflegends.com/v1/en_us/champions/${name}/index.json`)).data.champion;
 
-	result.push(`# \\<${cnc.title}\\> ${cnc.name}\r\n### \\<${U.firstUpCase(enc.title)}\\> ${U.firstUpCase(enc.name)}`);
+	result.push(`# &lt;${cnc.title}&gt; ${cnc.name}\r\n### &lt;${U.firstUpCase(enc.title)}&gt; ${U.firstUpCase(enc.name)}`);
 	result.push('');
 
-	let full = U.mixiFull(
+	let full = U.makePara(
 		cnc.biography.full.trim().split(/<\/p>/),
-		enc.biography.full.trim().split(/<\/p>/)
+		enc.biography.full.trim().split(/<\/p>/),
+		true
 	);
 
-	let quote = U.mixiQuote(
+	let quote = U.makeQuote(
 		cnc.biography.quote.trim(), cnc.biography['quote-author'] || cnc.name,
 		enc.biography.quote.trim(), enc.biography['quote-author'] || enc.name,
 	);
 
-	let short = U.mixiShort(
+	let short = U.makeShort(
 		cnc.biography.short.trim(),
 		enc.biography.short.trim()
 	);
 
-	return result.concat(quote).concat(short).concat(full);
+	return U.makeHead().concat(result).concat(quote).concat(short).concat(full).concat(U.makeTran()).concat(U.makeTail());
 };
 let compStoryA = async function(title) {
-	let result = [];
+	let header = [];
 
 	let cns = (await Axios.get(`https://yz.lol.qq.com/v1/zh_cn/story/${title}/index.json`)).data.story;
 	let ens = (await Axios.get(`https://universe-meeps.leagueoflegends.com/v1/en_us/story/${title}/index.json`)).data.story;
 
-	result.push(`# ${cns.title} ${U.firstUpCase(ens.title)}`);
-	result.push(`> 作者 Author: ${ens.subtitle.replace(/^by ?/, '')}`);
-	result.push('>');
-	result.push(`> 相关英雄 Related Champion: ${cns['story-sections'][0]['featured-champions'][0].name} (${ens['story-sections'][0]['featured-champions'][0].name})`);
-	result.push('>');
+	header.push(`# ${cns.title} ${U.firstUpCase(ens.title)}`);
+	header.push(`> 作者 Author: ${ens.subtitle.replace(/^by ?/, '')}`);
+	header.push('>');
+	header.push(`> 相关英雄 Related Champion: ${cns['story-sections'][0]['featured-champions'][0].name} (${ens['story-sections'][0]['featured-champions'][0].name})`);
+	header.push('>');
+
+	let counter = { start: 0 };
+
+	let full = [];
 
 	for(let i=0; i<cns['story-sections'][0]['story-subsections'].length;i++) {
-		let full = U.mixiStory(
-			cns['story-sections'][0]['story-subsections'][i].content.trim().replace(/<p><hr class='story-divider' \/><\/p>/g, '').split(/<\/i><\/p>/),
-			ens['story-sections'][0]['story-subsections'][i].content.trim().replace(/<p><hr class='story-divider' \/><\/p>/g, '').split(/<\/i><\/p>/)
-		);
-
-		result = result.concat(full);
+		full = full.concat(U.makeHara(
+			cns['story-sections'][0]['story-subsections'][i].content.trim().split(/<p><p><hr.*?story-divider.*?p><\/p>/g),
+			ens['story-sections'][0]['story-subsections'][i].content.trim().split(/<p><p><hr.*?story-divider.*?p><\/p>/g),
+			true,
+			false,
+			counter
+		));
 	}
 
-	return result;
+	return U.makeHead().concat(header).concat(U.makeCata(counter.start)).concat(full).concat(U.makeTran()).concat(U.makeTail());
 };
 
 (async function main() {
-	// _fs.writeFileSync('./Biography.md', (await compBiogA('syndra')).join('\r\n'));
+	// _fs.writeFileSync('./Biography.md', (await compBiogA('kaisa')).join('\r\n'));
 	_fs.writeFileSync('./Shtory.md', (await compStoryA('the-dreaming-pool')).join('\r\n'));
 })();
--- util.js
+++ util.js
@@ -1,149 +1,200 @@
+let articleDict = [ '传记 Biography', '短篇故事 Short Story', '漫画 Manga' ];
+let Moment = require('moment');
+
 let util = {
 	firstUpCase: function(str) {
 		return str.toLowerCase().replace(/( |^)[a-z]/g, function (head) { return head.toUpperCase(); } );
 	},
 
-	mixiSen: function(a = [], b = [], result = []) {
-		let max = a.length > b.length ? a.length : b.length;
+	// 混合句子
+	makeSent: function(cnSents = [], enSents = []) {
+		let result = [];
+
+		let max = cnSents.length > enSents.length ? cnSents.length : enSents.length;
 
 		for(let i=0; i<max; i++) {
-			let asen = (a[i] || '').trim();
-			let bsen = (b[i] || '').trim();
+			let cnSent = (cnSents[i] || '').trim();
+			let enSent = (enSents[i] || '').trim();
 
-			if(!asen && !bsen) {
+			if(!cnSent && !enSent) {
 				continue;
 			}
 
-			if(i != 0) { result.push('>'); }
+			result.push('>');
 
-			if(asen) {
-				result.push(asen.replace(/^/, '> ').replace(/$/, '。  '));
+			if(cnSent) {
+				result.push(cnSent.replace(/^/, '> ').replace(/$/, '。  '));
 			}
 
-			if(bsen) {
-				result.push(bsen.replace(/^/, '> ').replace(/$/, '.  '));
+			if(enSent) {
+				result.push(enSent.replace(/^/, '> ').replace(/$/, '.  '));
 			}
 		}
+
+		result.shift();
+
+		return result;
 	},
-	mixiQuote: function(cnq = '', cna = '', enq = '', ena = '') {
+	// 混合段落
+	makePara: function(cnParas = [], enParas = [], isMakeCata = false) {
+		let max = cnParas.length > enParas.length ? cnParas.length : enParas.length;
+
+		let result = [];
+		let numCata = 0;
+
+		for(let i=0; i<max; i++) {
+			let cnPara = (cnParas[i] || '').trim();
+			let enPara = (enParas[i] || '').trim();
+
+			if(!cnPara && !enPara) {
+				continue;
+			}
+
+			if(i != 0) { result.push(''); }
+
+			if(isMakeCata) {
+				result.push('#### '+ ('00'+(i+1)).slice(-2));
+
+				numCata++;
+			}
+
+			let cnSents;
+			let enSents;
+
+			if(cnPara) {
+				cnSents = cnPara.replace(/^<p>/, '').replace(/^<i>|<\/i>$/g, '').split(/[.。]/);
+			}
+
+			if(enPara) {
+				enSents = enPara.replace(/^<p>/, '').replace(/^<i>|<\/i>$/g, '').split(/[.。]/);
+			}
+
+			result = result.concat(util.makeSent(cnSents, enSents));
+		}
+
+		if(isMakeCata) {
+			result = util.makeCata(numCata).concat(result);
+		}
+
+		return result;
+	},
+	makeHara: function(cnHaras = [], enHaras = [], isCount = false, isMakeCata = false, numHaraWrap) {
+		let max = cnHaras.length > enHaras.length ? cnHaras.length : enHaras.length;
+
+		let result = [];
+		let numHara = numHaraWrap ? numHaraWrap.start : 0;
+
+		for(let i=0; i<max; i++) {
+			let cnPara = (cnHaras[i] || '').trim();
+			let enPara = (enHaras[i] || '').trim();
+
+			if(!cnPara && !enPara) {
+				continue;
+			}
+
+			if(i != 0) { result.push(''); }
+
+			if(isCount || isMakeCata) {
+				result.push('#### '+ ('00'+(++numHara)).slice(-2));
+			}
+
+			let cnParas;
+			let enParas;
+
+			if(cnPara) {
+				cnParas = cnPara.trim().split(/<\/p>/);
+			}
+
+			if(enPara) {
+				enParas = enPara.trim().split(/<\/p>/);
+			}
+
+			result = result.concat(util.makePara(cnParas, enParas));
+		}
+
+		if(numHaraWrap) {
+			numHaraWrap.start = numHara;
+		}
+
+		if(isMakeCata) {
+			result = util.makeCata().concat(result);
+		}
+
+		return result;
+	},
+
+	// 传记引述
+	makeQuote: function(cnQuote = '', cnAuthor = '', enQuote = '', enAuthor = '') {
 		let result = [];
 
-		result.push(`#### 引述 Quote by ${cna} (${ena})`);
+		result.push(`#### 引述 Quote by ${cnAuthor} (${enAuthor})`);
 
-		util.mixiSen(cnq.replace(/[“”"]/g, '').split(/[.。]/), enq.replace(/[“”"]/g, '').split(/[.。]/), result);
+		result = result.concat(util.makeSent(cnQuote.replace(/[“”"]/g, '').split(/[.。]/), enQuote.replace(/[“”"]/g, '').split(/[.。]/)));
 
 		result.push('');
 
 		return result;
 	},
-	mixiShort: function(cns = '', ens = '') {
+	// 传记简介
+	makeShort: function(cnShort = '', enShort = '') {
 		let result = [];
 
 		result.push('#### 简介 Short');
 
-		util.mixiSen(cns.split(/[.。]/), ens.split(/[.。]/), result);
+		result = result.concat(util.makeSent(cnShort.split(/[.。]/), enShort.split(/[.。]/)));
 
 		result.push('');
 
 		return result;
 	},
-	mixiFull: function(cnf = [], enf = []) {
-		let max = cnf.length > enf.length ? cnf.length : enf.length;
-
-		let result = [];
-		let vaildSec = 0;
-
-		for(let i=0; i<max; i++) {
-			let cnsens;
-			let ensens;
-
-			let cnsec = (cnf[i] || '').trim();
-			let ensec = (enf[i] || '').trim();
-
-			if(!cnsec && !ensec) {
-				continue;
-			}
-
-			if(i != 0) { result.push(''); }
-
-			result.push('#### '+ ('00'+(i+1)).slice(-2));
-
-			vaildSec++;
-
-			if(cnsec) {
-				cnsens = cnsec.replace(/^<p>/, '').split(/[.。]/);
-			}
-
-			if(ensec) {
-				ensens = ensec.replace(/^<p>/, '').split(/[.。]/);
-			}
-
-			util.mixiSen(cnsens, ensens, result);
-		}
-
-		// 目录
+	makeCata: function(count = 0) {
+		let result = [ '#### 目录 Catalog' ];
+		// 插入目录
 		let catalog = '> ';
-		for(let i=1; i<vaildSec+1; i++) {
+		for(let i=1; i<count+1; i++) {
 			let text = ('00'+i).slice(-2);
 			catalog += `[${text}](#${text}) | `;
 
-			if(!(i % 10)) {
+			if(!(i % 20)) {
 				catalog = catalog.replace(/ \| $/, '  \r\n> ');
 			}
+			else if(!(i % 10)) {
+				catalog = catalog.replace(/ \| $/, ' || ');
+			}
 		}
-		result.unshift(catalog.replace(/( \| | {2}\r\n> )$/, '\r\n'));
-		result.unshift('#### 目录 Catalog');
+
+		if(count) {
+			result = result.concat(catalog.replace(/( \| | {2}\r\n> )$/, '').split('\r\n'));
+		}
+
+		result.push('');
 
 		return result;
 	},
-	mixiStory: function(cnf = [], enf = []) {
-		let max = cnf.length > enf.length ? cnf.length : enf.length;
-
-		let result = [];
-		let vaildSec = 0;
-
-		for(let i=0; i<max; i++) {
-			let cnsens;
-			let ensens;
-
-			let cnsec = (cnf[i] || '').trim();
-			let ensec = (enf[i] || '').trim();
-
-			if(!cnsec && !ensec) {
-				continue;
-			}
-
-			if(i != 0) { result.push(''); }
-
-			result.push('#### '+ ('00'+(i+1)).slice(-2));
-
-			vaildSec++;
-
-			if(cnsec) {
-				cnsens = cnsec.replace(/^<p><i>/, '').split(/[.。]/);
-			}
-
-			if(ensec) {
-				ensens = ensec.replace(/^<p><i>/, '').split(/[.。]/);
-			}
-
-			util.mixiSen(cnsens, ensens, result);
-		}
-
-		// 目录
-		let catalog = '> ';
-		for(let i=1; i<vaildSec+1; i++) {
-			let text = ('00'+i).slice(-2);
-			catalog += `[${text}](#${text}) | `;
-
-			if(!(i % 10)) {
-				catalog = catalog.replace(/ \| $/, '  \r\n> ');
-			}
-		}
-		result.unshift(catalog.replace(/( \| | {2}\r\n> )$/, '\r\n'));
-		result.unshift('#### 目录 Catalog');
-
-		return result;
+	makeHead: function() {
+		return [
+			'● Runeterra Compare  ',
+			'***',
+			''
+		];
+	},
+	makeTran: function() {
+		return [
+			'',
+			'***',
+			'● 可供参考的直译  ',
+			'> 1: ',
+		];
+	},
+	makeTail: function(type = 0) {
+		return [
+			'',
+			'***',
+			'● github.com/LOLArranger/Runeterra-Compare  ',
+			`● ${articleDict[type]}  `,
+			`● Version ${Moment().format('YYYY.MM.DD')}  `,
+			'● All Based on League of Legends Universe(英雄联盟宇宙)  ',
+		];
 	}
 };
 
